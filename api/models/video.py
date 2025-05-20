5b7a06b693e83787c49c715c2ceaad6cad943fa1
Daniel Cañizares <147058221+danielcaag17@users.noreply.github.com>
2025-05-20 16:20:06 +0200

    Update video.py

--- api/models/video.py
+++ api/models/video.py
@@ -9,3 +9,10 @@ class Video(models.Model):
     s3_url = models.URLField(max_length=500)
     public_url = models.URLField(max_length=500, default='https://s3-us-west-2.amazonaws.com')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    '''
+    for now this, later when managing users, there would not be the default.
+    Now two random people cannot upload a video named equally
+    class Meta:
+        {title, autor}
+    '''
