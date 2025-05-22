b4421c189c46c360875affd6f586ae4bd66ab569
danielcaag <daniel.canizares@estudiantat.upc.edu>
2025-05-22 20:39:38 +0200

    Refactor with s3_utils

--- api/views/upload_video.py
+++ api/views/upload_video.py
@@ -3,10 +3,8 @@ from django.shortcuts import redirect
 import requests
 from urllib.parse import quote
 import uuid
-from django.conf import settings
 from django.utils.timezone import now
-import boto3
-import urllib.parse
+from ..utils import generate_public_url, upload_s3, convert_url
 
 '''
 If render had more resources, fastapi_url_post would be enough, 
@@ -16,41 +14,6 @@ fastapi_url_get = 'https://trackr-ml-api.onrender.com/api/video/response'
 fastapi_url_post = 'https://trackr-ml-api.onrender.com/api/video/detect'
 
 FASTAPI_GET = True
-
-
-def convert_url(s3_url):
-    parsed_url = urllib.parse.urlparse(s3_url)
-
-    # Check if the URL corresponds to an S3 bucket correctly
-    if parsed_url.netloc.endswith("amazonaws.com"):
-        bucket_name = parsed_url.netloc.split('.')[0]
-        file_path = parsed_url.path.lstrip('/')
-
-        s3_url = f"s3://{bucket_name}/{file_path}"
-        return s3_url
-    else:
-        raise ValueError("The URL is not a valid S3 bucket.")
-
-
-def upload_s3(video_file, video_id, user):
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_S3_REGION_NAME
-    )
-
-    s3_file_key = f"original_videos/{video_file.name}"
-
-    s3.upload_fileobj(
-        video_file,
-        settings.AWS_STORAGE_BUCKET_NAME,
-        s3_file_key,
-        ExtraArgs={'ContentType': 'video/mp4'}
-    )
-
-    s3_url = f"https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{s3_file_key}"
-    return s3_url
 
 
 def get_method():
@@ -70,32 +33,6 @@ def post_method(video_file, video_id, user):
     }
     response = requests.post(fastapi_url_post, json=request_body)
     return s3_url, response
-
-
-def generate_public_url(video_file):
-    # Create s3 client
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_S3_REGION_NAME
-    )
-
-    # Adapt the name as it is saved in s3
-    name = video_file.name.split('.')[0]
-    name = name + "_detected.mp4"
-    s3_file_key = f"detections/{name}"
-
-    # Generar URL firmada (válida por 1 hora)
-    public_url = s3.generate_presigned_url(
-        ClientMethod='get_object',
-        Params={
-            'Bucket': settings.AWS_STORAGE_BUCKET_NAME,
-            'Key': s3_file_key
-        },
-        ExpiresIn=3600
-    )
-    return public_url
 
 
 def upload_video(request):
